1e02aa1f1e60516801e8cc20d7193ffd88e41e36
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-07-25 15:59:47 +0300

    chore: Update CustomResponse class
    - Add method for bad request responses
    - Make options argument optional
    - Make data argument optional

--- src/api/response.ts
+++ src/api/response.ts
@@ -2,26 +2,57 @@ import { type IResponse } from '../types';
 import { Response } from 'express';
 
 class CustomResponse {
-	static notFound<T>(res: Response, options: IResponse<T>): IResponse<T> {
-		return res.json({
-			message: options.message || 'Not Found',
-			status: options.status || 404,
-			data: options.data || {},
+	// Private method to handle the response logic
+	private static sendResponse<T>(
+		res: Response,
+		statusCode: number,
+		message: string,
+		data: T,
+	): IResponse<T> {
+		return res.status(statusCode).json({
+			message,
+			status: statusCode,
+			data,
 		}) as unknown as IResponse<T>;
 	}
-	static success<T>(res: Response, options: IResponse<T>): IResponse<T> {
-		return res.json({
-			message: options.message || 'Success',
-			status: options.status || 200,
-			data: options.data || {},
-		}) as unknown as IResponse<T>;
+
+	static notFound<T>(res: Response, options?: IResponse<T>): IResponse<T> {
+		return CustomResponse.sendResponse<T>(
+			res,
+			options?.status || 404,
+			options?.message || 'Not Found',
+			options?.data || ({} as T),
+		);
 	}
-	static unauthorized<T>(res: Response, options: IResponse<T>): IResponse<T> {
-		return res.json({
-			message: options.message || 'Unauthorized',
-			status: options.status || 401,
-			data: options.data || {},
-		}) as unknown as IResponse<T>;
+
+	static badRequest<T>(res: Response, options?: IResponse<T>): IResponse<T> {
+		return CustomResponse.sendResponse<T>(
+			res,
+			options?.status || 400,
+			options?.message || 'Bad Request',
+			options?.data || ({} as T),
+		);
+	}
+
+	static success<T>(res: Response, options?: IResponse<T>): IResponse<T> {
+		return CustomResponse.sendResponse<T>(
+			res,
+			options?.status || 200,
+			options?.message || 'Success',
+			options?.data || ({} as T),
+		);
+	}
+
+	static unauthorized<T>(
+		res: Response,
+		options?: IResponse<T>,
+	): IResponse<T> {
+		return CustomResponse.sendResponse<T>(
+			res,
+			options?.status || 401,
+			options?.message || 'Unauthorized',
+			options?.data || ({} as T),
+		);
 	}
 }
 
