f7458d8fc2cb3ba937959fdcbbf13c03a19f1e5e
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-07-25 15:47:05 +0300

    feat: Update GET all projects controller
    - Use ProjectManagement model to return all projects
    - Implement CustomResponse class to optimize response handling

--- src/routes/project.routes.ts
+++ src/routes/project.routes.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import { getProjects, getProjectByID } from '../controllers/project.controller';
+import { getProjects } from '../controllers/project.controller';
 
 const router = Router();
 
 router.get('/', getProjects);
-router.get('/:projectId/', getProjectByID);
 
 export default router;
